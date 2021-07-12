a5e17c098260816e2fc0ad85285b3974182a939f
murilo-toddy <murilo.todao@usp.br>
2021-07-12 20:03:06 -0300

    Adição de informações em seq. pacotes p/ seq. ação

--- include/SequenciaPacotes.h
+++ include/SequenciaPacotes.h
@@ -1,10 +1,24 @@
 #ifndef SEQUENCIAPACOTES_H
 #define SEQUENCIAPACOTES_H
 
+#define QUANTIDADE_PACOTES  32
+
+#define PACOTE_BRANCO       0
+#define PACOTE_PRETO        1
+#define PACOTE_AMARELO      2
+#define PACOTE_VERMELHO     3
+#define PACOTE_VERDE        4
+#define PACOTE_AZUL         5
+
+typedef float retornoCalcFitness;
+
 class SequenciaPacotes {
     private:
 
     public:
+
+    
+    retornoCalcFitness calculaFitness(int *sequenciaAcoes);
     
 };
 
